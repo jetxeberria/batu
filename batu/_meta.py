da3ae2f164e041ed20ffc72fdc75920aedbc4e19
jetxeberria-dasnano <jetxeberria@das-nano.com>
2022-06-14 20:06:26 +0200

    First commit #0

---
+++ batu/_meta.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""batu package metadata"""
+
+__title__ = "batu"
+__description__ = "Kalkulagailua Arduinorekin eskaerak batzeko"
+__url__ = "https://github.com/jetxeberria/batu"
+__version__ = "0.1.0"
+__author__ = "jetxeberria"
+__email__ = "etxeberria_92@hotmail.com"
+__license__ = "Copyright"
+__copyright__ = "Copyright (c) la-unica, 2022"
+__status__ = "Production"
